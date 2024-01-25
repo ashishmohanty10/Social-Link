7b1f52b12e6a6da1f680505f5c87280203c86cf4
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-01-25 17:49:53 +0530

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import Profile from "./assets/Profile.jpg";
+import { List } from "./components/LinkList";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="container h-screen w-full flex justify-center items-center">
+        <div className="bg-darkGrey w-[375px] h-[600px] rounded-lg">
+          <div className="flex flex-col justify-start items-center">
+            <div className="flex justify-center items-center mt-5 mb-2 w-[100px] overflow-hidden rounded-full object-center">
+              <img src={Profile} alt="" className="w-[200px]" />
+            </div>
+
+            <div className="my-3 text-center">
+              <h1 className="font-bold text-white text-xl">Ashish Mohanty</h1>
+              <p className="font-semibold text-sm text-primary mb-4">
+                India, West Bengal
+              </p>
+
+              <p className="font-base text-sm text-white">
+                "A passionate frontend developer from India{" "}
+                <span className="text-primary font-medium"> (🇮🇳)</span>"
+              </p>
+            </div>
+
+            <div className="flex flex-col justify-center items-center my-3">
+              {List.map(({ href, title }) => (
+                <li
+                  className="cursor-pointer list-none text-sm uppercase py-2 px-10 items-stretch bg-grey rounded-lg font-semibold text-white hover:bg-primary hover:text-offBlack duration-100 mb-5 w-[300px] text-center"
+                  key={href}
+                >
+                  <a href={href} target="_blank">
+                    {title}
+                  </a>
+                </li>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/components/LinkList.jsx
+++ src/components/LinkList.jsx
@@ -1,6 +1,25 @@
 export const List = [
   {
     title: "github",
+    href: "https://github.com/ashishmohanty10",
+  },
+
+  {
+    title: "twitter",
     href: "https://twitter.com/tw_sk1llz",
+  },
+
+  {
+    title: "Youtube",
+    href: "https://www.youtube.com/channel/UC7Mf0bywhvkQ_UkGX-Fmu6g",
+  },
+
+  {
+    title: "instagram",
+    href: "https://www.instagram.com/ig.ashishmohanty/?hl=en",
+  },
+  {
+    title: "linkedin",
+    href: "https://www.linkedin.com/in/ashish---mohanty/",
   },
 ];
