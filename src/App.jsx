38834ab401d0440b7fb8eace53dbd184ad1e50ea
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-01-26 23:14:52 +0530

    slight changes done in ui

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,39 @@ import { List } from "./components/LinkList";
 const App = () => {
   return (
     <div>
-      <div className="container h-screen w-full flex justify-center items-center">
+      <div className="container flex items-center justify-center w-full h-screen">
         <div className="bg-darkGrey w-[375px] h-[550px] rounded-lg">
-          <div className="flex flex-col justify-start items-center">
+          <div className="flex flex-col items-center justify-start">
             <div className="flex justify-center items-center mt-5 mb-2 w-[100px] overflow-hidden rounded-full object-center">
               <img src={Profile} alt="" className="w-[200px]" />
             </div>
 
             <div className="my-3 text-center">
-              <h1 className="font-bold text-white text-xl">Ashish Mohanty</h1>
-              <p className="font-semibold text-sm text-primary mb-4">
+              <h1 className="text-xl font-bold text-white">Ashish Mohanty</h1>
+              <p className="mb-4 text-sm font-semibold text-primary">
                 India, West Bengal
               </p>
 
-              <p className="font-base text-sm text-white">
+              <p className="text-sm text-white font-base">
                 "A passionate frontend developer from India{" "}
-                <span className="text-primary font-medium"> (🇮🇳)</span>"
+                <span className="font-medium text-primary"> (🇮🇳)</span>"
               </p>
             </div>
 
-            <div className="flex flex-col justify-center items-center my-3">
+            <div className="flex flex-col items-center justify-center my-3">
               {List.map(({ href, title, img }) => (
                 <a
                   href={href}
                   target="_blank"
-                  className="group cursor-pointer grid grid-cols-2 justify-evenly gap-x-2 items-start w-[300px]  py-2 px-10 bg-grey rounded-lg mb-5 hover:bg-primary duration-100:"
+                  className="group cursor-pointer grid grid-cols-2 justify-center  items-start w-[250px]  py-2 px-10 bg-grey rounded-lg mb-5 hover:bg-primary duration-100:"
                   key={href}
                 >
-                  <div className="flex justify-center items-center">
+                  <div className="flex items-center justify-center">
                     <img src={img} alt="" className="w-[20px]" />
                   </div>
 
                   <li
-                    className="group-hover:text-offBlack list-none text-sm uppercase  items-stretch font-semibold text-white group-[hover:text-offblack]"
+                    className="group-hover:text-offBlack list-none text-sm uppercase font-semibold text-white group-[hover:text-offblack] tracking-normal"
                     key={href}
                   >
                     {title}
